88afc207f022420efb21bd687b3db325508c9e33
KevinMidboe <kevin.midboe@gmail.com>
2017-12-05 11:14:04 +0100

    Shortened down a bit.

--- luke_5/gullrekka.py
+++ luke_5/gullrekka.py
@@ -3,36 +3,25 @@
 # @Author: KevinMidboe
 # @Date:   2017-12-04 16:40:56
 # @Last Modified by:   KevinMidboe
-# @Last Modified time: 2017-12-05 10:36:22
+# @Last Modified time: 2017-12-05 11:13:52
 
-total_loops = 0
-
-def iterate(gold_row, num):
-   global total_loops
-   try:
-      countdown = gold_row[num-1]
-   except IndexError:
-      countdown = num
-
-   tmp = []
-   while countdown >= 1:
-      tmp.append(num)
-      countdown -=1
-      total_loops +=1
-      if (total_loops >= 1000000):
-         return tmp
-   return tmp
+def gold():
+   gold_row = [1]
+   number = 1
+   while True:
+      countdown = gold_row[number-1]
+      while countdown > 0:
+         gold_row.append(number)
+         countdown -=1
+         if len(gold_row) >= 1000000:
+            return gold_row
+      number +=1
 
 def main():
-   global total_loops
-   gold_row = []
-   i = 1
-   while total_loops < 1000000:
-      gold_row.extend(iterate(gold_row, i))
-      i +=1
+   row = gold()
 
    print('Finished with million iterations')
-   print(sum(gold_row))
+   print(sum(row)+1)
 
 if __name__ == '__main__':
    main()
